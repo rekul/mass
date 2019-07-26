6038f0805658cfbb3b523b64d4ad371e2b6f1240
rekul <haydnluker@gmail.com>
2019-07-25 20:40:22 -0500

    'oops'

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,36 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+
+  app.get("/api/user/", function(req, res) {
+    db.User.findAll(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
+  app.post("/api/user/create", function(req, res) {
+    db.User.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
+
+  app.post("/api/user/login", function(req, res) {
+    db.User.findOne({where:{
+      username:req.body.username,
+      password:req.body.password,
+    }}).then(function(dbExample) {
+      if(!dbExample){
+        console.log('user not found');
+        res.json(-1);
+        app.locals.user = -1;
+      }else{
+        console.log(dbExample);
+        res.json(dbExample);
+        app.locals.user = dbExample.id;
+
+      }
+    });
+  });
+
+ 
 };
